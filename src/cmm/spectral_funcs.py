120844b246f456ae95f2c2073054bdf904f88645
vrutten <vms.rutten@gmail.com>
2023-04-01 10:57:39 -0400

    add coherence function (tested)

--- src/cmm/spectral_funcs.py
+++ src/cmm/spectral_funcs.py
@@ -1,12 +1,262 @@
 import jax.numpy as np
-from utils import _triage_segments, make_chunks
+from cmm.utils import _triage_segments, make_chunks
 from numpy import fft as sp_fft
 from jax.lax import scan
 from jax.lax import broadcast
 
 
+def estimate_spectrum_from_coefs(
+    coefs_xknf: np.array, coefs_yknf=None, alltoall=True, abs=True
+):
+    xn = coefs_xknf.shape[0]
+    kn = coefs_xknf.shape[1]
+    wn = coefs_xknf.shape[-1]
+
+    if coefs_yknf is not None:
+        yn = coefs_yknf.shape[0]
+        fxy = lambda v, y: foldxy(v, y, alltoall)
+        if alltoall:
+            init = np.zeros([xn, yn, wn]).astype("complex64")
+        else:
+            init = np.zeros([xn, wn]).astype("complex64")
+        pxy, _ = scan(fxy, init, (coefs_xknf, coefs_yknf))
+    else:
+        fxy = lambda v, y: fold(v, y, alltoall)
+        if alltoall:
+            init = np.zeros([xn, xn, wn]).astype("complex64")
+        else:
+            init = np.zeros([xn, wn]).astype("complex64")
+        pxy, _ = scan(fxy, init, coefs_xknf)
+
+    pxy /= kn  # average over k
+
+    if abs:
+        pxy = np.abs(pxy)
+
+    return pxy
+
+
+def foldxy(carry, x, alltoall):
+    x0 = x[0]
+    y0 = x[1]
+    xn = x0.shape[0]
+    yn = y0.shape[0]
+    if alltoall:
+        tmp0 = broadcast(x0, sizes=(yn,))
+        tmp1 = broadcast(y0, sizes=(xn,))
+        tmp0 = tmp0.transpose([1, 0, 2])
+    else:
+        tmp0 = x0
+        tmp1 = y0
+    carry += tmp0 * np.conj(tmp1)  # nx x ny x w
+    return carry, 0
+
+
+def fold(carry, x0, alltoall):
+    xn = x0.shape[0]
+    if alltoall:
+        tmp0 = broadcast(x0, sizes=(xn,))
+        tmp1 = tmp0.transpose([1, 0, 2])
+    else:
+        tmp0 = x0
+        tmp1 = x0
+    carry += tmp0 * np.conj(tmp1)  # nx x ny x w
+    return carry, 0
+
+
+def compute_spectral_coefs(
+    xnt: np.ndarray,
+    fs=1.0,
+    window="hann",
+    nperseg=None,
+    noverlap=None,
+    detrend="constant",
+    return_onesided=True,
+    scaling="spectrum",
+    axis=-1,
+    freq_minmax=[0, np.inf],
+    nfft=None,
+):
+    """Compute Spectral Fourier Coefs"""
+
+    def detrend_func(d):
+        from scipy.signal import signaltools
+
+        return signaltools.detrend(d, type=detrend, axis=-1)
+
+    n, t = xnt.shape
+    win, nperseg = _triage_segments(
+        window, nperseg, input_length=t
+    )  # win is a 1d array
+
+    if nfft is None:
+        nfft = nperseg
+    elif nfft < nperseg:
+        raise ValueError("nfft must be greater than or equal to nperseg.")
+    else:
+        nfft = int(nfft)
+
+    if noverlap is None:
+        noverlap = nperseg // 2
+    else:
+        noverlap = int(noverlap)
+    if noverlap >= nperseg:
+        raise ValueError("noverlap must be less than nperseg.")
+
+    if return_onesided:
+        if np.iscomplexobj(xnt):
+            sides = "twosided"
+        else:
+            sides = "onesided"
+    else:
+        sides = "twosided"
+
+    if sides == "twosided":
+        freqs = sp_fft.fftfreq(nfft, 1 / fs)
+    elif sides == "onesided":
+        freqs = sp_fft.rfftfreq(nfft, 1 / fs)
+
+    if scaling == "density":
+        scale = 1.0 / (fs * (win * win).sum())
+    elif scaling == "spectrum":
+        scale = 1.0 / win.sum() ** 2
+    else:
+        raise ValueError("Unknown scaling: %r" % scaling)
+
+    if detrend is False:
+        detrend_func = None
+    coefs_xnkf = myfft_helper(xnt, win, detrend_func, nperseg, noverlap, nfft, sides)
+    coefs_xknf = coefs_xnkf.transpose([1, 0, 2])
+    coefs_xknf *= scale
+
+    valid_freqs = (np.abs(freqs) >= freq_minmax[0]) & (np.abs(freqs) <= freq_minmax[1])
+    freqs = freqs[valid_freqs]
+    coefs_xknf = coefs_xknf[:, :, valid_freqs]
+
+    return coefs_xknf, freqs
+
+
+def myfft_helper(
+    x: np.ndarray,
+    win: np.ndarray,
+    detrend_func,
+    nperseg: int,
+    noverlap: int,
+    nfft,
+    sides: str,
+):
+    """
+    Calculate windowed FFT, for internal use by
+    `scipy.signal._spectral_helper`.
+    This is a helper function that does the main FFT calculation for
+    `_spectral helper`. All input validation is performed there, and the
+    data axis is assumed to be the last axis of x. It is not designed to
+    be called externally. The windows are not averaged over; the result
+    from each window is returned.
+    Returns
+    -------
+    result : ndarray
+        Array of FFT data
+    Notes
+    -----
+    Adapted from matplotlib.mlab
+    .. versionadded:: 0.16.0
+    """
+    # Created strided array of data segments
+    if nperseg == 1 and noverlap == 0:
+        result = x[..., np.newaxis]
+    else:
+        result = make_chunks(x, nperseg, noverlap)
+
+    # Detrend each data segment individually
+    if detrend_func is not None:
+        result = detrend_func(result)  # default to last axis - result is N x K x T
+
+    # Apply window by multiplication
+
+    result = win * result
+
+    # Perform the fft. Acts on last axis by default. Zero-pads automatically
+    if sides == "twosided":
+        func = sp_fft.fft
+    else:
+        result = (
+            result.real
+        )  # forces result to be real (should be real anyway as doing one sided fft...)
+        func = sp_fft.rfft
+    coefs_nkf = func(result, n=nfft)
+
+    return coefs_nkf
+
+
+def compute_coherence(
+    xnt: np.array,
+    ynt=None,
+    fs=1.0,
+    window="hann",
+    nperseg=None,
+    noverlap=None,
+    nfft=None,
+    detrend="constant",
+    return_onesided=True,
+    scaling="spectrum",
+    axis=-1,
+    freq_minmax=[0, np.inf],
+    abs=False,
+    alltoall=True,  # every x with every y
+):
+    pxx_nk, freq = estimate_spectrum(
+        xnt=xnt,
+        fs=fs,
+        window=window,
+        nperseg=nperseg,
+        noverlap=noverlap,
+        nfft=nfft,
+        detrend=detrend,
+        return_onesided=return_onesided,
+        scaling=scaling,
+        axis=axis,
+        freq_minmax=freq_minmax,
+        abs=True,
+        alltoall=False,
+    )
+    pyy_mk, freq = estimate_spectrum(
+        xnt=ynt,
+        fs=fs,
+        window=window,
+        nperseg=nperseg,
+        noverlap=noverlap,
+        nfft=nfft,
+        detrend=detrend,
+        return_onesided=return_onesided,
+        scaling=scaling,
+        axis=axis,
+        freq_minmax=freq_minmax,
+        abs=True,
+        alltoall=False,
+    )
+    pxy_nmk, freq = estimate_spectrum(
+        xnt=xnt,
+        ynt=ynt,
+        fs=fs,
+        window=window,
+        nperseg=nperseg,
+        noverlap=noverlap,
+        nfft=nfft,
+        detrend=detrend,
+        return_onesided=return_onesided,
+        scaling=scaling,
+        axis=axis,
+        freq_minmax=freq_minmax,
+        abs=True,
+        alltoall=True,
+    )
+    coherence = pxy_nmk**2 / (pxx_nk[:, None] * pyy_mk[None])
+    return coherence, freq
+
+
 def estimate_spectrum(
-    xnt: np.ndarry,
+    xnt: np.array,
     ynt=None,
     fs=1.0,
     window="hann",
@@ -97,7 +347,7 @@ def estimate_spectrum(
     if not x_in_coefs:
         if len(xnt.shape) < 2:
             xnt = broadcast(xnt, sizes=(1,))
-        xn = xn.shape[0]
+        xn = xnt.shape[0]
         coefs_xknf, freqs = compute_spectral_coefs(
             xnt=xnt,
             fs=fs,
@@ -123,7 +373,7 @@ def estimate_spectrum(
                 ynt = broadcast(ynt, sizes=(1,))
             yn = ynt.shape[0]
             coefs_yknf, _ = compute_spectral_coefs(
-                xnt=xnt,
+                xnt=ynt,
                 fs=fs,
                 window=window,
                 nperseg=nperseg,
@@ -152,6 +402,7 @@ def estimate_spectrum(
             init = np.zeros([xn, yn, wn]).astype("complex64")
         else:
             init = np.zeros([xn, wn]).astype("complex64")
+
         pxy, _ = scan(fxy, init, (coefs_xknf, coefs_yknf))
     else:
         fxy = lambda v, y: fold(v, y, alltoall)
@@ -161,161 +412,9 @@ def estimate_spectrum(
             init = np.zeros([xn, wn]).astype("complex64")
         pxy, _ = scan(fxy, init, coefs_xknf)
 
-    pxy /= kn  # average over k
+    # pxy /= kn  # average over k
 
     if abs:
         pxy = np.abs(pxy)
 
     return pxy, freqs
-
-
-def foldxy(carry, x, alltoall):
-    x0 = x[0]
-    y0 = x[1]
-    xn = x0.shape[0]
-    yn = y0.shape[0]
-    if alltoall:
-        tmp0 = broadcast(x0, sizes=(yn,))
-        tmp1 = broadcast(y0, sizes=(xn,))
-        tmp0 = tmp0.transpose([1, 0, 2])
-    else:
-        tmp0 = x0
-        tmp1 = y0
-    carry += tmp0 * np.conj(tmp1)  # nx x ny x w
-    return carry, 0
-
-
-def fold(carry, x0, alltoall):
-    xn = x0.shape[0]
-    if alltoall:
-        tmp0 = broadcast(x0, sizes=(yn,))
-        tmp1 = tmp0.transpose([1, 0, 2])
-    else:
-        tmp0 = x0
-        tmp1 = x0
-    carry += tmp0 * np.conj(tmp1)  # nx x ny x w
-    return carry, 0
-
-
-def compute_spectral_coefs(
-    xnt: np.ndarray,
-    fs=1.0,
-    window="hann",
-    nperseg=None,
-    noverlap=None,
-    detrend="constant",
-    return_onesided=True,
-    scaling="spectrum",
-    axis=-1,
-    freq_minmax=[0, np.inf],
-    nfft=None,
-):
-    """Compute Spectral Fourier Coefs"""
-
-    def detrend_func(d):
-        from scipy.signal import signaltools
-
-        return signaltools.detrend(d, type=detrend, axis=-1)
-
-    pass
-
-    n, t = xnt.shape
-    win, nperseg = _triage_segments(
-        window, nperseg, input_length=t
-    )  # win is a 1d array
-
-    if nfft is None:
-        nfft = nperseg
-    elif nfft < nperseg:
-        raise ValueError("nfft must be greater than or equal to nperseg.")
-    else:
-        nfft = int(nfft)
-
-    if noverlap is None:
-        noverlap = nperseg // 2
-    else:
-        noverlap = int(noverlap)
-    if noverlap >= nperseg:
-        raise ValueError("noverlap must be less than nperseg.")
-
-    if return_onesided:
-        if np.iscomplexobj(x):
-            sides = "twosided"
-        else:
-            sides = "onesided"
-    else:
-        sides = "twosided"
-
-    if sides == "twosided":
-        freqs = sp_fft.fftfreq(nfft, 1 / fs)
-    elif sides == "onesided":
-        freqs = sp_fft.rfftfreq(nfft, 1 / fs)
-
-    if scaling == "density":
-        scale = 1.0 / (fs * (win * win).sum())
-    elif scaling == "spectrum":
-        scale = 1.0 / win.sum() ** 2
-    else:
-        raise ValueError("Unknown scaling: %r" % scaling)
-
-    coefs_xnkf = myfft_helper(xnt, win, detrend_func, nperseg, noverlap, nfft, sides)
-    coefs_xknf = coefs_xnkf.transpose([1, 0, 2])
-    coefs_xknf *= scale
-
-    valid_freqs = (freqs >= freq_minmax[0]) & (freqs <= freq_minmax[1])
-    freqs = freqs[valid_freqs]
-    coefs_xknf = coefs_xknf[:, :, valid_freqs]
-
-    return coefs_xknf, freqs
-
-
-def myfft_helper(
-    x: np.ndarray,
-    win: np.ndarray,
-    detrend_func: function,
-    nperseg: int,
-    noverlap: int,
-    nfft,
-    sides: str,
-):
-    """
-    Calculate windowed FFT, for internal use by
-    `scipy.signal._spectral_helper`.
-    This is a helper function that does the main FFT calculation for
-    `_spectral helper`. All input validation is performed there, and the
-    data axis is assumed to be the last axis of x. It is not designed to
-    be called externally. The windows are not averaged over; the result
-    from each window is returned.
-    Returns
-    -------
-    result : ndarray
-        Array of FFT data
-    Notes
-    -----
-    Adapted from matplotlib.mlab
-    .. versionadded:: 0.16.0
-    """
-    # Created strided array of data segments
-    if nperseg == 1 and noverlap == 0:
-        result = x[..., np.newaxis]
-    else:
-        result = make_chunks(x, nperseg, noverlap)
-
-    # Detrend each data segment individually
-    result = detrend_func(result)  # default to last axis - result is N x K x T
-
-    # Apply window by multiplication
-
-    result = win * result
-
-    # Perform the fft. Acts on last axis by default. Zero-pads automatically
-    if sides == "twosided":
-        func = sp_fft.fft
-    else:
-        result = (
-            result.real
-        )  # forces result to be real (should be real anyway as doing one sided fft...)
-        func = sp_fft.rfft
-    coefs_nkf = func(result, n=nfft)
-
-    return coefs_nkf
