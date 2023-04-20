f372208d9d6637dec9a4e3459f94054624a1ee53
Michael Innerberger <innerbergerm@hhmi.org>
2023-04-20 17:45:00 -0400

    Improve custom svd

--- src/cmm/custom_svd.py
+++ src/cmm/custom_svd.py
@@ -1,18 +1,24 @@
 from scipy.sparse.linalg import LinearOperator, eigsh
 
-
 def compute_first_singular_vector(A):
     """This is taken from scipy.sparse.linalg's implementation of svds"""
 
     n, m = A.shape
     if n >= m:
-        XH_X = LinearOperator(shape=(n, n), dtype=A.dtype,
-                              matmat=lambda X: A.conj().transpose() @ (A @ X),
-                              matvec=lambda x: A.conj().transpose() @ (A @ x))
+        def matmat(X):
+            return A.conj().transpose() @ (A @ X)
+        def matvec(x):
+            return A.conj().transpose() @ (A @ x)
+
     else:
-        XH_X = LinearOperator(shape=(n, n), dtype=A.dtype,
-                              matmat=lambda X: A @ (A.conj().transpose() @ X),
-                              matvec=lambda x: A @ (A.conj().transpose() @ x))
+        def matmat(X):
+            return A @ (A.conj().transpose() @ X)
+        def matvec(x):
+            return A @ (A.conj().transpose() @ x)
+
+    XH_X = LinearOperator(shape=(n, n), dtype=A.dtype,
+                          matmat=matmat,
+                          matvec=matvec)
 
     eigval, eigvec = eigsh(XH_X, k=1, tol=0, which='LM')
     return eigval, eigvec
